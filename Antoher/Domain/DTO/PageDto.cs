33f31614c9c3e12818d2540e0177e3f84ee0f574
sanokkk <sashaevstr2002@yandex.ru>
2023-01-29 14:37:34 +0300

    added page DTO for pagination

---
+++ Antoher/Domain/DTO/PageDto.cs
@@ -0,0 +1,12 @@
+﻿using Antoher.Domain.Models;
+using System.Collections.Generic;
+
+namespace Antoher.Domain.DTO
+{
+    public class PageDto
+    {
+        public List<Post> page { get; set; }
+
+        public int pages { get; set; }
+    }
+}
